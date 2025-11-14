a619bfec1cdc23c0bd8ca9f15406c691cffdf612
skajmaks <wojtaszczyk71@gmail.com>
2025-11-14 12:32:06 +0100

    5.6

--- src/components/CContents.jsx
+++ src/components/CContents.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import './CContents.css';
+import jsonData from '../res/text.json';
 
-class CContents extends Component{
+class CContents extends Component {
 
     constructor(){
         super()
         this.state = {msg: 'Witaj użytkowniku. Kliknij przycisk aby poznać wiadomość'}
+        this.txtData = () => JSON.parse(JSON.stringify(jsonData));
     }
 
     changeMsg(){
@@ -12,15 +15,26 @@ class CContents extends Component{
     }
 
     render(){
-        return(
-            <div>
-                <h1>{this.state.msg}</h1>
-                <button onClick={() => this.changeMsg()}>
-                    Kliknij, aby poznać tajemnice
-                </button>
+    const items = [];
+    for(let i=0; i<this.txtData().count; i++ ){
+        let value = this.txtData().text[i];
+        items.push(
+            <div id={"div" + (i+1)} className="contentsFlex">
+                <img className="contentsFlexImg" src={"./img/img" + (i+1) + ".png"} alt=""/>
+                <p className="contentsFlexTxt">{value}</p>
             </div>
-        )
+        );
     }
+
+    return (
+        <div className="contentsDivClass">
+            {items}
+            <h1>{this.state.msg}</h1>
+            <button onClick={() => this.changeMsg()}>Kliknij na przycisk, aby poznac tajemnice</button>
+        </div>
+    )
+    }
+
 }
 
 export default CContents;
