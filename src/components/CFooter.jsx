41cd98a86d98e47eab290a45931533be73fb553e
skajmaks <wojtaszczyk71@gmail.com>
2025-11-14 01:35:56 +0100

    5.3

--- src/components/CFooter.jsx
+++ src/components/CFooter.jsx
@@ -1,4 +1,22 @@
 import React from "react";
+import './CFooter.css';
+
+
+export const CFooter = () => {
+  let date = new Date();
+
+  return React.createElement(
+    "div",
+    { className: "footerDivClass" },
+    React.createElement(
+      "h5",
+      { id: "footerText", className: "footerTextClass" },
+      "Prog. JS, przykładowy serwis React. Dzisiaj mamy: " +
+        date.toLocaleDateString("pl-PL")
+    )
+  );
+};
+
 
 // stara wersja zakomentowana
 // function CFooter(){
@@ -13,17 +31,17 @@ import React from "react";
 // }
 
 // NOWA WERSJA – FUNKCJA STRZAŁKOWA
-export const CFooter = () => {
-    let date = new Date();
-    return(
-        <div>
-            <h5>
-                Prog. JS, przykładowy serwis React. Dzisiaj mamy: {" "}
-                {date.toString()}{ " " }
-            </h5>
-        </div>
-    );
-}
+//export const CFooter = () => {
+//    let date = new Date();
+  //  return(
+    //    <div>
+      //      <h5>
+        //        Prog. JS, przykładowy serwis React. Dzisiaj mamy: {" "}
+          //      {date.toString()}{ " " }
+            //</h5>
+        //</div>
+    //);
+//}
 
 // export default CFooter;
 
